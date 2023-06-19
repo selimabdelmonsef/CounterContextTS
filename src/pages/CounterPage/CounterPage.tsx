bb77285b0f043bc97cd05f54394b79e5cda65784
Selim Abdelmonsef <selim.mohamed@btech.com>
2023-06-19 14:21:19 +0200

    feat: can not decrement if count is 0

--- src/pages/CounterPage/CounterPage.tsx
+++ src/pages/CounterPage/CounterPage.tsx
@@ -5,14 +5,20 @@ import { StyledCountBtns } from './CounterPage.styles';
 export const Counter = () => {
   const { count, increment, decrement } = useCounter();
 
+  const onDecrement = () => {
+    if (count !== 0 || count > 0) {
+      decrement();
+    }
+  };
+
   return (
     <>
       <h1>Count: {count}</h1>
       <div>
-        <StyledCountBtns background="increment" onClick={increment}>
+        <StyledCountBtns background="increment" onClick={() => increment()}>
           +
         </StyledCountBtns>
-        <StyledCountBtns onClick={decrement}>-</StyledCountBtns>
+        <StyledCountBtns onClick={onDecrement}>-</StyledCountBtns>
       </div>
     </>
   );
